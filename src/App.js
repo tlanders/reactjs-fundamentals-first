76c8284e325163fc093d4ec4dfeebd0bc0723873
tlanders <768713+tlanders@users.noreply.github.com>
2020-07-05 16:00:09 -0500

    added an add new profile button with callback

--- src/App.js
+++ src/App.js
@@ -27,21 +27,33 @@ class App extends React.Component {
         {name : 'Bill', age: 41, bio: 'Shoots at everything', hobbies : ['shooting']},
       ]
     };
+
+    this.addUser = this.addUser.bind(this);
+  }
+
+  addUser(event) {
+      let user = {name : 'Tim', age: 27, bio: 'Reads a lot', hobbies : ['reading', 'spelling', 'pontificating']};
+      this.setState({
+          "profiles": this.state.profiles.concat(user)
+      });
   }
 
   render() {
       let profiles = this.state.profiles.map(profile => {
           return (
-              <Profile name={profile.name}
-                   age={profile.age}
-                   bio={profile.bio}
-                   hobbies={profile.hobbies}
-              />
+              <div>
+                  <Profile name={profile.name}
+                       age={profile.age}
+                       bio={profile.bio}
+                       hobbies={profile.hobbies}
+                  />
+              </div>
           )
       })
     return (
         <div className="App">
             {profiles}
+            <button onClick={this.addUser}>Add new profile</button>
         </div>
     );
   }
