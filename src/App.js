3464fe51cc1069164d242c63f85b95758865541d
tlanders <768713+tlanders@users.noreply.github.com>
2020-07-05 16:15:18 -0500

    reformatted code

--- src/App.js
+++ src/App.js
@@ -6,44 +6,44 @@ import Profile from './components/Profile';
 import AddProfile from './components/AddProfile';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      profiles: [
-        {name : 'Sue', age: 32, bio: 'Likes to swim and stuff', hobbies : ['swimming', 'biking']},
-        {name : 'Bill', age: 41, bio: 'Shoots at everything', hobbies : ['shooting']},
-      ]
-    };
+    constructor(props) {
+        super(props);
+        this.state = {
+            profiles: [
+                {name: 'Sue', age: 32, bio: 'Likes to swim and stuff', hobbies: ['swimming', 'biking']},
+                {name: 'Bill', age: 41, bio: 'Shoots at everything', hobbies: ['shooting']},
+            ]
+        };
 
-    this.addUser = this.addUser.bind(this);
-  }
+        this.addUser = this.addUser.bind(this);
+    }
 
-  addUser(event) {
-      let user = {name : 'Tim', age: 27, bio: 'Reads a lot', hobbies : ['reading', 'spelling', 'pontificating']};
-      this.setState({
-          "profiles": this.state.profiles.concat(user)
-      });
-  }
+    addUser(event) {
+        let user = {name: 'Tim', age: 27, bio: 'Reads a lot', hobbies: ['reading', 'spelling', 'pontificating']};
+        this.setState({
+            "profiles": this.state.profiles.concat(user)
+        });
+    }
 
-  render() {
-      let profiles = this.state.profiles.map(profile => {
-          return (
-              <div>
-                  <Profile name={profile.name}
-                       age={profile.age}
-                       bio={profile.bio}
-                       hobbies={profile.hobbies}
-                  />
-              </div>
-          )
-      })
-    return (
-        <div className="App">
-            {profiles}
-            <AddProfile addUser={this.addUser}/>
-        </div>
-    );
-  }
+    render() {
+        let profiles = this.state.profiles.map(profile => {
+            return (
+                <div>
+                    <Profile name={profile.name}
+                             age={profile.age}
+                             bio={profile.bio}
+                             hobbies={profile.hobbies}
+                    />
+                </div>
+            )
+        })
+        return (
+            <div className="App">
+                {profiles}
+                <AddProfile addUser={this.addUser}/>
+            </div>
+        );
+    }
 }
 
 export default App;
