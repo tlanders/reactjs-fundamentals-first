0823c58d77ee307c45f31bb282a6a1e1f0526a71
tlanders <768713+tlanders@users.noreply.github.com>
2020-07-05 13:03:20 -0500

    Getting started

--- src/App.js
+++ src/App.js
@@ -2,25 +2,31 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      profiles: [
+        {name : 'Sue', age: 32, bio: 'Likes to swim and stuff', hobbies : ['swimming', 'biking']},
+        {name : 'Bill', age: 41, bio: 'Shoots at everything', hobbies : ['shooting']},
+      ]
+    };
+  }
+
+  render() {
+    return (
+        <div className="App">
+          <h3>Bill</h3>
+          <p>
+            Bill is 40 years old. Likes to shoot.
+          </p>
+          <h3>Hobbies</h3>
+          <ul>
+            <li>Shooting</li>
+          </ul>
+        </div>
+    );
+  }
 }
 
 export default App;
